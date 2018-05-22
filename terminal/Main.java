de11dcdd1fd79e12621410814143cd14ff538da9
Paul Tonning <paul@dotclientsolutions.com>
2018-05-22 05:56:31 -0700

    update README.md

--- terminal/Main.java
+++ terminal/Main.java
@@ -1,6 +1,13 @@
+import jssc.SerialPort; 
+import jssc.SerialPortEvent; 
+import jssc.SerialPortEventListener; 
+import jssc.SerialPortException;
+import java.io.UnsupportedEncodingException;
+
 import javax.swing.*;  
 import java.awt.event.*;  
 public class Main implements ActionListener{  
+    static SerialPort serialPort;
     JLabel l1,l2;  
     JTextArea area;  
     JButton b;  
@@ -28,6 +35,67 @@ public class Main implements ActionListener{
         l2.setText("Characters: "+text.length());  
     }  
     public static void main(String[] args) {  
-        new Main();  
+       serialPort = new SerialPort("/dev/tty.usbmodem1441"); 
+       try {
+           serialPort.openPort();//Open port
+           serialPort.setParams(9600, 8, 1, 0); //Set params
+           int mask = SerialPort.MASK_RXCHAR; //Prepare the event mask
+           serialPort.setEventsMask(mask); //Set mask
+           serialPort.addEventListener(new SerialPortReader()); //Add SerialPortEventListener
+       }
+       catch (SerialPortException ex) {
+           System.out.println(ex);
+       }
+
+       new Main();  
     }  
-}  
+/*
+ * In this class the method serialEvent must be implemented, through it we learn about 
+ * events that happened to our port. But we will not report on all 
+ * events. We will only report those events that we put in the mask. 
+ * In this case the arrival of the data.
+ */
+static class SerialPortReader implements SerialPortEventListener {
+    private String b;
+    public SerialPortReader()
+    {
+       b = new String("");
+    }
+
+    public void printBuffer(byte buffer[]) throws UnsupportedEncodingException
+    {
+        String str = new String(buffer,"UTF-8");
+        System.out.println(str);
+    }
+
+    public void serialEvent(SerialPortEvent event) {
+        if(event.isRXCHAR()){//If data is available
+                //Read data, if 10 bytes available 
+                try {
+                    byte buffer[] = serialPort.readBytes(1);
+                    if(buffer[0] == '\n')
+                    {
+                           System.out.println(b);
+                           b = "";
+                    }
+                    else 
+                    {
+                           try 
+                           {
+                              b += new String(buffer,"UTF-8");
+                           }
+                           catch(UnsupportedEncodingException ex)
+                           {
+                              System.out.println(ex);
+                           }
+                    }
+                }
+                catch (SerialPortException ex) 
+                {
+                  System.out.println(ex);
+                }
+        }
+    }
+}
+
+}   // end of Main class definition
